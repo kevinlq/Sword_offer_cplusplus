a8b6092ccb1c9614108eca2ee2208037ad150a9f
kevinlq0912 <kevinlq0912@163.com>
2017-08-05 15:55:55 +0800

    change

--- SwordOffer/SwordOffer/main.cpp
+++ SwordOffer/SwordOffer/main.cpp
@@ -8,6 +8,20 @@ int main(int argc,char *argv[])
 	Solution1 s1;
 	s1.test();
 
+	int a[3][4];
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			a[i][j] = i * 4 + j;
+		}
+	}
+
+	for (int i = 0; i < 12; i++)
+	{
+		printf("a[%d] = %d\n",i,*(*a + i));
+	}
+
 	system("pause");
 	return 0;
 }
